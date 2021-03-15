a73925e2439049657f171ef031649f235ac949e8
Draylon Vieira <draylid@gmail.com>
2021-03-15 20:03:14 -0300

    update

--- Op.py
+++ Op.py
@@ -1,5 +1,11 @@
 import cv2
 import numpy as np
+
+def mod(n):
+    if(n>0):
+        return n
+    else:
+        return -1*n
 class OpImage:
     
     def janela(image,x,y,n):
@@ -16,7 +22,7 @@ class OpImage:
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
                 if image[i][j] >= threshold:
-                    image[i][j] = 1
+                    image[i][j] = 255
                 else:
                     image[i][j] = 0
         return image
@@ -28,7 +34,9 @@ class OpImage:
                 val+= filter1[i][j]*wind[i][j]
         return val
 
-    def convolucao(image,filter,n):
+    def convolucao2(image,filter,n):
+        d1 = (int(image.shape[0]/n),int(image.shape[1]/n))
+        cv2.resize(image,d1)
         lm = int(n-1-(n-1)/2)
         for i in range(lm,image.shape[0]-lm,2*lm):
             for j in range(lm,image.shape[1]-lm,2*lm):
@@ -39,6 +47,70 @@ class OpImage:
 
                 #convoluted_image[i][j]=OpImage.calcular_convolucao(image,OpImage.janela(image,i,j,n))
         return image
+    
+    def convolucao(src,filter,n):
+        d1 = (int(src.shape[0]/n),int(src.shape[1]/n))
+        image = cv2.resize(src,d1)
+        lm = int(n-1-(n-1)/2)
+        for i in range(lm,image.shape[0]-lm,2*lm):
+            for j in range(lm,image.shape[1]-lm,2*lm):
+                image[i][j]=OpImage.calcular_convolucao(src,OpImage.janela(src,i,j,n))
+        return image
+
+    def rescale(output,min,max):
+        min1,max1=0,0
+        dist1 = max - min
+        for i in output:
+            for j in i:
+                if j > max1:
+                    max1=j
+                if j < min1:
+                    min1=j
+        dist2 = mod(max1) - mod(min1)
+        dist = dist1 / dist2
+        for i in range(len(output)):
+            for j in range(len(output[0])):
+                output[i][j]*=dist
+                output[i][j]-=min1
+        return output
+        
+        
+        
+
+
+    def convolve(image, kernel):
+        # grab the spatial dimensions of the image, along with
+        # the spatial dimensions of the kernel
+        (iH, iW) = image.shape[:2]
+        (kH, kW) = kernel.shape[:2]
+        # allocate memory for the output image, taking care to
+        # "pad" the borders of the input image so the spatial
+        # size (i.e., width and height) are not reduced
+        pad = (kW - 1) // 2
+        image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+        output = np.zeros((iH, iW), dtype="float32")
+        # loop over the input image, "sliding" the kernel across
+        # each (x, y)-coordinate from left-to-right and top to
+        # bottom
+        for y in np.arange(pad, iH + pad):
+            for x in np.arange(pad, iW + pad):
+                # extract the ROI of the image by extracting the
+                # *center* region of the current (x, y)-coordinates
+                # dimensions
+                roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+                # perform the actual convolution by taking the
+                # element-wise multiplicate between the ROI and
+                # the kernel, then summing the matrix
+                k = (roi * kernel).sum()
+                # store the convolved value in the output (x,y)-
+                # coordinate of the output image
+                output[y - pad, x - pad] = k
+        # rescale the output image to be in the range [0, 255]
+        #output = rescale_intensity(output, in_range=(0, 255))
+        output = OpImage.rescale(output, 0,255)
+        output = (output * 255).astype("uint8")
+        # return the output image
+        return output
 
 
     def grayscale(image):
--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import cv2
+import numpy as np
 from Op import OpImage
 
-image1 = cv2.imread("./images/casa.png")
+image = cv2.imread("./images/casa.png")
 
-#l1 = image1.janela(image1.getMidWidth(),image1.getMidHeight(),3)
+#d1=( image.shape[0]*3,image.shape[1]*2 )
+#image = cv2.resize(image,d1)
 
-grayscale = OpImage.grayscale(image1.copy())
+#l1 = image.janela(image.getMidWidth(),image.getMidHeight(),3)
 
-print(grayscale[5][5])
+gray = OpImage.grayscale(image)
 
-thold50 = OpImage.threshold(grayscale.copy(),200)
-
-print(thold50[5][5])
+#thold50 = OpImage.threshold(grayscale.copy(),200)
 
 #f1 = [[-1,0,1],[-2,0,2],[-1,0,1]]
-f1 = [[-1,-2,-1],[0,0,0],[1,2,1]]
+#f1 = [[-1,-2,-1],[0,0,0],[1,2,1]]
 #f1 = [[0,1,0],[0,0,0],[0,0,0]]
 #f1 = [[0,1,0],[1,0,1],[0,1,0]]
 #f1 = [[-1,-1,0],[-1,8,1],[-1,-1,-1]]
 
-conv = OpImage.convolucao(thold50.copy(),f1,3)
+#conv = OpImage.convolucao(thold50.copy(),f1,3)
 
 
 #============
 
-cv2.imshow('original',image1)
-cv2.imshow('grayscale',grayscale)
-cv2.imshow('Threshold',thold50)
-cv2.imshow('convoluted',conv)
+#cv2.imshow('original',image)
+#cv2.imshow('grayscale',grayscale)
+#cv2.imshow('Threshold',thold50)
+#cv2.imshow('convoluted',conv)
 
-cv2.waitKey(0)
+#cv2.waitKey(0)
+
+#cv2.destroyAllWindows()
 
 
-cv2.destroyAllWindows()
 
 '''
-
 Parte A: implemente uma função “janela deslizante” que a partir da localização (i,j) 
 do centro de  uma  janela  NxN  (N  ímpar),  retorna  as  coordenadas  dos  vizinhos 
 de  (i,j). Inicialmente faça para N=3.
@@ -46,3 +46,61 @@ posicionamento de janela deslizante que vc implementou na Parte-A, anterior
 
 '''
 
+# construct the argument parse and parse the arguments
+# construct average blurring kernels used to smooth an image
+smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
+# construct a sharpening filter
+sharpen = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
+
+# construct the Laplacian kernel used to detect edge-like
+# regions of an image
+laplacian = np.array((
+	[0, 1, 0],
+	[1, -4, 1],
+	[0, 1, 0]), dtype="int")
+# construct the Sobel x-axis kernel
+sobelX = np.array((
+	[-1, 0, 1],
+	[-2, 0, 2],
+	[-1, 0, 1]), dtype="int")
+# construct the Sobel y-axis kernel
+sobelY = np.array((
+	[-1, -2, -1],
+	[0, 0, 0],
+	[1, 2, 1]), dtype="int")
+
+
+# construct the kernel bank, a list of kernels we're going
+# to apply using both our custom `convole` function and
+# OpenCV's `filter2D` function
+kernelBank = (
+	("small_blur", smallBlur),
+	("large_blur", largeBlur),
+	("sharpen", sharpen),
+	("laplacian", laplacian),
+	("sobel_x", sobelX),
+	("sobel_y", sobelY)
+)
+
+
+# load the input image and convert it to grayscale
+
+
+# loop over the kernels
+for (kernelName, kernel) in kernelBank:
+	# apply the kernel to the grayscale image using both
+	# our custom `convole` function and OpenCV's `filter2D`
+	# function
+	print("[INFO] applying {} kernel".format(kernelName))
+	convoleOutput = OpImage.convolve(gray, kernel)
+	opencvOutput = cv2.filter2D(gray, -1, kernel)
+	# show the output images
+	cv2.imshow("original", gray)
+	cv2.imshow("{} - convole".format(kernelName), convoleOutput)
+	cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
